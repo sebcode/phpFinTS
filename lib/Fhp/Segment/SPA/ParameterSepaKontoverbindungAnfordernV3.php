a724439c6298f0485fc370b34bd43f5693d5c6d3
Sebastian Volland <seb@baunz.net>
2024-04-28 18:11:08 +0200

    Add support for "Segmentversion 3"
    https://github.com/nemiah/phpFinTS/issues/391

---
+++ lib/Fhp/Segment/SPA/ParameterSepaKontoverbindungAnfordernV3.php
@@ -0,0 +1,14 @@
+<?php
+/** @noinspection PhpUnused */
+
+namespace Fhp\Segment\SPA;
+
+/**
+ * Data Element Group: Parameter SEPA-Kontoverbindung anfordern (Version 3)
+ *
+ * @link https://www.hbci-zka.de/dokumente/spezifikation_deutsch/fintsv3/FinTS_3.0_Messages_Geschaeftsvorfaelle_2015-08-07_final_version.pdf
+ * Section: D (letter P)
+ */
+class ParameterSepaKontoverbindungAnfordernV3 extends ParameterSepaKontoverbindungAnfordernV2
+{
+}
